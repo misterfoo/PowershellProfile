14e37479f87e35804c839e2da320b53dbb73f15c
Charles Nevill <charles.nevill@gmail.com>
2020-01-08 13:06:04 -0600

    Improvements to tab completion...

--- TabCompletion.ps1
+++ TabCompletion.ps1
@@ -1,5 +1,4 @@
 
-
 function TabExpansion2
 {
 
@@ -27,16 +26,29 @@ Param(
 
 End
 {
-	if ($psCmdlet.ParameterSetName -ne 'ScriptInputSet')
-    {
-        return [System.Management.Automation.CommandCompletion]::CompleteInput(
-            $ast, $tokens, $positionOfCursor, $options)
-	}
+	Set-StrictMode -Version Latest
 
-	# run the default tab completion. we need this if for no other reason than the fact
-	# that we can't construct a CommandCompletion instance ourselves.
-	$result = [System.Management.Automation.CommandCompletion]::CompleteInput(
-		$inputScript, $cursorColumn, $options)
+   	function log( $thing )
+   	{
+		$thing >> d:\temp\log.txt
+   	}
+
+	trap
+	{
+		log $_.ToString()
+		log $_.ScriptStackTrace
+   	}
+
+	log "--------------------------------------"
+	log ""
+   	
+	# parse input
+	if ($PSCmdlet.ParameterSetName -eq 'ScriptInputSet') {
+		$_ = [System.Management.Automation.CommandCompletion]::MapStringInputToParsedInput($inputScript, $cursorColumn)
+		$ast = $_.Item1
+		$tokens = $_.Item2
+		$positionOfCursor = $_.Item3
+	}
 
 	# gets the last command on the line (using ; as the separator)
 	function Get-LastPart( [string] $line )
@@ -47,6 +59,27 @@ End
 		{ $line.Substring( $lastBreak + 1 ).TrimStart() }
 		else
 		{ $line }
+	}
+
+	# finds the root directory of a git repository, assuming we are currently in a subdirectory
+	function Find-GitRoot
+	{
+		$dir = (Get-Location).Path
+		for(;;)
+		{
+			if( Test-Path (Join-Path $dir ".git") )
+			{
+				return $dir
+			}
+
+			$parent = Split-Path -Parent $dir
+			if( $parent -eq "" )
+			{
+				return $null
+			}
+
+			$dir = $parent
+		}
 	}
 
 	# Gets the list of local branches, as a set of objects looking like:
@@ -73,10 +106,10 @@ End
 
 	# Gets the status of the local git repository, as a set of objects looking like:
 	# Stat = the status of the file (M, A, or ??)
-	# File = the name of the file
+	# File = the path and name of the file (note that paths are relative to repository root)
 	function Get-GitStatus
 	{
-		switch -regex ((git status -z) -split "\0")
+		switch -regex (git status --porcelain=v1)
 		{
 			"(..) (.*)"
 			{
@@ -85,35 +118,89 @@ End
 		}
 	}
 
+	# Filters the $results array to entries beginning with $prefix
+	function FilterResults( $results, $prefix )
+	{
+		if( $prefix -ne "" )
+		{
+			$pattern = "^" + [system.Text.RegularExpressions.Regex]::Escape( $prefix )
+			$results = $results | where { $_ -match $pattern }
+		}
+		$results
+	}
+
 	# gets custom completion values that should come ahead of default completions
 	function Get-CustomCompletionPre
 	{
 		$last = Get-LastPart $inputScript
 
 	    # auto-complete git branches
-	    if( $last -match "^\s*git\s+(checkout|branch)" )
+	    #
+	    #					   git   command               -args     prefix-of-thing
+	    if( $last -match "^\s*git\s+(checkout|branch)\s+(?:-\w+\s+)*(.*)" )
 	    {
-	        Get-GitBranches | select -Expand Name
-	        return
+	        $names = Get-GitBranches | select -Expand Name
+	        return FilterResults $names $matches[2]
 	    }
 
 	    # auto-complete files for index operations
-	    if( $last -match "^\s*git\s+(add|stage|restore)" )
+		#
+	    #					   git    command                -args     prefix-of-thing
+	    if( $last -match "^\s*git\s+(add|stage|restore)\s+(?:-\w+\s+)*(.*)" )
 	    {
-	        Get-GitStatus | select -Expand File
-	        return
+			# Have they already typed some of the file they want? If so, skip tab
+			# completion if they started with .\ or ..\ or a drive letter
+			$prefix = $matches[2]
+			if( ($prefix -match "^\.") -or ($prefix -match "^\w:\\") )
+			{
+				return @()
+			}
+	    
+	        $files = Get-GitStatus | select -Expand File
+
+			# The output of Get-GitStatus is relative to the root of the repo, but we want
+			# tab completion to function relative to the current directory
+			$root = Find-GitRoot
+			if( $root -ne $null )
+			{
+				$pwd = (Get-Location).Path
+				$relative = $pwd.Substring( $root.Length + 1 )
+				log "adjusting files relative to $relative"
+				$files = $files | % {
+						if( $_.StartsWith( $relative ) )
+						{
+							log "relative: $_"
+							$_.Substring( $relative.Length + 1 ) # +1 is for final path separator
+						}
+						else
+						{
+							log "absolute: $_"
+							$root + $_
+						}	
+					}
+			}
+
+	        return FilterResults $files $matches[2]
 	    }
 	}
-
+	
 	# see if any of our custom completion handlers match
-	$custom = @(Get-CustomCompletionPre | % { New-Object Management.Automation.CompletionResult $_ })
-	if( $custom.Length -ne 0 )
+	$custom = @(Get-CustomCompletionPre)
+	if( $custom.Length -gt 0 )
 	{
-		$result.CompletionMatches.Clear()
-		$custom | % { $result.CompletionMatches.Add( $_ ) }
+		# make a fake local version of TabExpansion which only returns the matches we want
+		# see this repo for helpful guidance: https://github.com/nightroman/FarNet/blob/master/PowerShellFar/TabExpansion2.ps1
+		function TabExpansion { $custom }
+
+		# run "regular" tab expansion with our custom results
+		return [System.Management.Automation.CommandCompletion]::CompleteInput(
+				$ast, $tokens, $positionOfCursor, $null)
 	}
 
-	$result
+
+	# run the default tab completion
+	[System.Management.Automation.CommandCompletion]::CompleteInput(
+		$inputScript, $cursorColumn, $options)
 }
 
 }
